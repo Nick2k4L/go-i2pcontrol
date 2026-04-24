cb8e9d55b36032ca2b382df104787e9c8c9f0eb9
Nicholas Lopez <nico2k4@gmail.com>
2026-04-24 12:22:29 -0700

    Add: base functions

--- tunnelmanager.go
+++ tunnelmanager.go
@@ -1,1 +1,27 @@
 package i2pcontrol
+
+type HiddenService struct{}
+
+func GetHiddenService(name string) (map[string]interface{}, error) {
+	return nil, nil
+}
+
+func DeleteHiddenService(name string) (string, error) {
+	return "", nil
+}
+
+func StopAllServices() (string, error) {
+	return "", nil
+}
+
+func StartAllServices() (string, error) {
+	return "", nil
+}
+
+func RestartAllServices() (string, error) {
+	return "", nil
+}
+
+func AddHiddenService(service HiddenService) (string, error) {
+	return "", nil
+}
